205803586025583cbb9b7adeff25341fff9e0142
Ruslan Semen <ruslansemen20@gmail.com>
2020-05-15 15:51:11 +0300

    Node modul fs()

--- JS part 2 практика/Lesson_7_1/script.js
+++ JS part 2 практика/Lesson_7_1/script.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 fs.readFile('./data.json', 'utf-8', (err, data) => {
     if (!err) {
         const obj = JSON.parse(data);
+        console.log(obj.first + " " + obj.second + " " + obj.third);
+        obj.third = "THREE";
         console.log(obj);
+        fs.writeFile('./data2.json', "[{\"hello\": \"Node JS!\"}]", (err) => {});
+        fs.writeFile('./data3.json', JSON.stringify(obj), (err) => {});
     }
-})
+});
