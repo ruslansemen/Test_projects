18fbe044ed44c5b7bc281a8fc0b7c616406fa638
Ruslan Semen <ruslansemen20@gmail.com>
2020-06-07 15:12:14 +0300

    07.06.2020/1

---
+++ ReactJS/2/app.js
@@ -0,0 +1,23 @@
+"use strict";
+// деструктуризация объекта -----------------------------
+//let {var1, var2} = {var1: …, var2: …};
+let options = {
+  title: "Меню",
+  width: 100,
+  height: 200,
+};
+
+let { height: h, title, width: w } = options;
+console.log(title);
+console.log(w);
+console.log(h);
+console.log(options);
+
+// деструктуризация массива ----------------------------------
+// let [firstName, lastName] = ["Илья", "Кантор"];
+// let mas = ["Илья", "Кантор", "Юлий", "Цезарь"];
+// let [firstName, , , thirdName] = mas;
+// console.log(firstName); // Илья
+//console.log(lastName); // Кантор
+// console.log(thirdName); // Кантор
+// console.log(mas);
